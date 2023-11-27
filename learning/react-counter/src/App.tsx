1511e17f792c8f214d741a25b2757dc3477c471c
gsalunke <ganeshharshil@gmail.com>
2023-11-27 11:54:01 +0530

    added css

--- learning/react-counter/src/App.tsx
+++ learning/react-counter/src/App.tsx
@@ -1,23 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [seconds, setSeconds] = React.useState<number>(0);
+  const [toggle, setToggle] = React.useState<boolean>(false);
+
+  const ref = React.useRef<NodeJS.Timeout | null>(null);
+
+  const toggleStopwatch = () => {
+    setToggle(!toggle);
+  };
+
+  const resetStopwatch = () => {
+    setToggle(false);
+    setSeconds(0);
+  };
+
+  React.useEffect(() => {
+    ref.current = setInterval(() => {
+      if (toggle) setSeconds((state) => state + 1);
+    }, 1000);
+
+    return () => {
+      if (ref.current) clearInterval(ref.current);
+    };
+  }, [toggle]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="counter">
+          <div>{seconds}</div>
+
+          <div className="buttons">
+            <button onClick={toggleStopwatch}>
+              {toggle ? "Stop" : "Start"}
+            </button>
+
+            <button onClick={resetStopwatch} className="reset">
+              Reset
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   );
